bc9cbe5d5cf6bc9128b335556c80eddde2a1af06
Metehan Homris <94820668+homris@users.noreply.github.com>
2023-08-17 03:07:41 -0700

    Added main file.

---
+++ main.py
@@ -0,0 +1,13 @@
+import random, os, sys
+
+nums = [*range(1, 7)]
+
+def main():
+    input("\nPress ENTER to roll the dice:\n")
+    dice1 = random.choice(nums)
+    dice2 = random.choice(nums)
+    print(str(dice1) + " : " + str(dice2))
+    os.execl(sys.executable, sys.executable, *sys.argv) #restarts the program
+
+
+main()
